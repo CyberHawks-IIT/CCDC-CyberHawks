6aa7957356d61851c70e3afd2ff52d4577edb378
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-02-19 16:58:33 -0600

    Add cron and hash, initial kill_service

---
+++ linux/kill_service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for var in "$@"
+do
+	systemctl stop "$var"
+	systemctl disable "$var"
+	systemctl mask "$var"
+	file = $(systemctl show -P FragmentPath "$var")
+	if [ -n "$file" ]; then
+		mv "$file" "$file.bak"
+		echo "$file" >> killed_services
+	else
+		echo "$var" >> killed_services
+	fi
+done
+systemctl daemon-reload
